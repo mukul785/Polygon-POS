d95f4c666863febf33851ee5b0f862713a8befd7
mukul785 <156766802+mukul785@users.noreply.github.com>
2024-08-20 11:51:15 +0530

    update

--- src/components/MintNft.js
+++ src/components/MintNft.js
@@ -1,13 +1,22 @@
 import React from "react";
+import './MintNft.css';
 import atm_abi from "../artifacts/contracts/MyNFT.sol/MyNFT.json";
 
 const MintNft = ({account, contractAddress}) => {
     return(
         <>
-        <div>
-            <h1>Mint </h1>
-            <p>{account}</p><br />
-            <p>{contractAddress}</p>
+        <div className="mint-main">
+            <div className="head">Mint NFT</div>
+            <div className="input-area">
+                <div className="in-div">
+                    <div className="head-in">Enter URL:</div>
+                    <input type="text" name="URL" className="text-in" id="UrlNft" />
+                </div>
+                <div className="in-div">
+                    <div className="head-in">Enter Description:</div>
+                    <input type="text" name="Description" id="descriptionNft" />
+                </div>
+            </div>
         </div>
         </>
     );
